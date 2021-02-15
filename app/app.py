42bca5481f9f65c146475ad388ea2d94a3389b80
Sam Miller <sjmill@uw.edu>
2021-02-15 13:43:13 -0800

    Changes to add dropdown menu to main tab

--- app/app.py
+++ app/app.py
@@ -9,13 +9,51 @@ from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
 
-data = load.load_folder('.\data\Clean UW')
-df = data[0]
+
+class DataObj:
+    def __init__(self, input_data_path):
+
+        self.data = load.load_folder(input_data_path)
+        self.params = self.get_params()
+
+    def get_params(self):
+        """
+        Return a list of parameters
+
+        Notes
+        -----
+        Uses the first data frame columns to determine parameters.
+        """
+        params = []
+        df = self.data[0]
+        for i in df.columns:
+            if i in [
+                'Dp > 0.3',
+                'Dp > 0.5',
+                'Dp > 1.0',
+                'Dp > 2.5',
+                'Dp > 5.0',
+                'Dp > 10.0',
+                'PM1_Std',
+                'PM2.5_Std',
+                'PM10_Std',
+                'PM1_Env',
+                'PM2.5_Env',
+                'PM10_Env',
+                'Temp(C)',
+                'RH( %)',
+                'P(hPa)',
+                'Alti(m)',
+                'Noise (dB)'
+            ]:
+                params.append(i)
+        return params
+
+data_obj = DataObj('.\data\Clean UW')
+df = data_obj.data[0]
 
 fig1 = figures.map_figure(df)
 fig2 = figures.line_figure(df)
-
-
 
 
 
@@ -62,7 +100,7 @@ def update_click_output(button_click, close_click):
 )
 def render_tab_content(tab_switch):
     if tab_switch == "tab1":
-        return layouts.build_tab(fig1)
+        return layouts.build_overview_tab(data_obj, fig1)
     else:
         return layouts.build_tab(fig2)
 
--- app/layouts.py
+++ app/layouts.py
@@ -1,5 +1,4 @@
 
-
 import dash_core_components as dcc
 import dash_html_components as html
 
@@ -29,9 +28,43 @@ def build_tabs():
         ],
     )
 
-def build_tab(fig):
-    return html.Div(dcc.Graph(
+def build_overview_tab(data_obj, fig):
+    return html.Div(
+        id="tabs",
+        className="tabs",
+        children=[
+            html.P(),  # this creates a new paragraph
+            html.H5('Parameter'),
+            dcc.Dropdown(id='param-drop'
+                         , options=[
+                            {'label': i, 'value': i} for i in data_obj.params],
+                         value=['US'],
+                         multi=True
+                         ),
+            dcc.Graph(
                         id='Graph1',
                         figure=fig
                     ),
+
+                ]
             )
+
+#
+# @app.callback(Output('province-drop', 'options'),
+#               [Input('country-drop', 'value')])
+# def set_province_options(country):
+#     if len(country) > 0:
+#         countries = country
+#         return [{'label': i, 'value': i} for i in sorted(set(df['province'].loc[df['country'].isin(countries)]))]
+#
+#     else:
+#         countries = []
+#         return [{'label': i, 'value': i} for i in sorted(set(df['province'].loc[df['country'].isin(countries)]))]
+
+
+def build_tab(fig):
+    return html.Div(dcc.Graph(
+                         id='Graph1',
+                         figure=fig
+                     ),
+             )
