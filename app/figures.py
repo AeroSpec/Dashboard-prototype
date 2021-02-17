8cd61cbfb261bf1351f7f6c6787a01e306bed7c9
zlavogb <zlavogb@uw.edu>
2021-02-17 00:32:07 -0800

    add floorplan figure and fix image resizing distortion

--- app/figures.py
+++ app/figures.py
@@ -9,16 +9,15 @@ def map_figure(data, params =[]):
     fig = go.Figure()
 
     # Constants
-    img_width = 1200
-    img_height = 1200
-    scale_factor = 0.5
+    img_width = 890
+    img_height = 890
 
     # Add invisible scatter trace.
     # This trace is added to help the autoresize logic work.
     fig.add_trace(
         go.Scatter(
-            x=[0, img_width * scale_factor],
-            y=[0, img_height * scale_factor],
+            x=[0, img_width],
+            y=[0, img_height],
             mode="markers",
             marker_opacity=0
         )
@@ -27,55 +26,50 @@ def map_figure(data, params =[]):
     # Configure axes
     fig.update_xaxes(
         visible=False,
-        range=[0, img_width * scale_factor]
     )
 
     fig.update_yaxes(
         visible=False,
-        range=[0, img_height * scale_factor],
-        # the scaleanchor attribute ensures that the aspect ratio stays constant
-        scaleanchor="x"
     )
 
     # Add image
     fig.add_layout_image(
-        dict(
-            x=0,
-            sizex=img_width * scale_factor,
-            y=img_height * scale_factor,
-            sizey=img_height * scale_factor,
-            xref="x",
-            yref="y",
-            opacity=1.0,
-            layer="below",
-            sizing="stretch",
-            source="https://wcs.smartdraw.com/office-floor-plan/examples/office-floor-plan.png?bn=15100111771")
+        x=0,
+        sizex=img_width,
+        y=img_height,
+        sizey=img_height,
+        xref="x",
+        yref="y",
+        opacity=1.0,
+        # TODO: change to use png file in repo
+        source="https://wcs.smartdraw.com/office-floor-plan/examples/office-floor-plan.png?bn=15100111771"
     )
 
     # Configure other layout
     fig.update_layout(
-        width=img_width * scale_factor,
-        height=img_height * scale_factor,
+        width=img_width,
+        height=img_height,
         margin={"l": 0, "r": 0, "t": 0, "b": 0},
+        plot_bgcolor='rgba(0,0,0,0)'
     )
 
     fig.add_shape(type="circle",
                   xref="x", yref="y",
                   fillcolor="#f70000",
                   line_color="#f70000",
-                  x0=300, y0=300, x1=320, y1=320,
+                  x0=500, y0=490, x1=530, y1=520,
                   )
     fig.add_shape(type="circle",
                   xref="x", yref="y",
                   fillcolor="#fff980",
                   line_color="#fff980",
-                  x0=100, y0=100, x1=120, y1=120,
+                  x0=130, y0=140, x1=160, y1=170,
                   )
     fig.add_shape(type="circle",
                   xref="x", yref="y",
                   fillcolor="#137506",
                   line_color="#137506",
-                  x0=120, y0=440, x1=140, y1=460,
+                  x0=180, y0=620, x1=210, y1=650,
                   )
 
     return fig
