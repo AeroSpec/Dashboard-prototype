11124ecb07222764a9cff0d7ce84622a1d8fb2a8
Sam Miller <sjmill@uw.edu>
2021-02-19 23:00:24 -0800

    Adding a stats column and improving layout

--- app/app.py
+++ app/app.py
@@ -7,10 +7,32 @@ import data
 import os
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 
+
+
+def notifications():
+    return html.Div(
+        [
+            dbc.Button(
+                "Notification",
+                id="auto-toast-toggle",
+                color="primary",
+                className="notification",
+            ),
+            dbc.Toast(
+                [html.P("The sensors have gone haywire!", className="mb-0")],
+                id="auto-toast",
+                header="Alert",
+                icon="primary",
+                duration=4000,
+                dismissable=True,
+            ),
+        ]
+    )
 
 
 
@@ -22,14 +44,36 @@ df = data_obj.data[id1]['data']
 fig1 = figures.map_figure(df)
 fig2 = figures.line_figure(df)
 
+def stats_panel():
+    return html.Div(
+        id="quick-stats",
+        className="row",
+        children=[
+            html.Div(
+                id="card-1",
+                children=[
+                    html.P("Key Data"),
+                ],
+            ),
+            html.Div(
+                id="card-2",
+                children=[
+                    html.P("More Data"),
+                ],
+            ),
+            html.Div(
+                id="notifications-card",
+                children=notifications()
+            ),
+        ],
+        #style={'float': 'right', 'margin': 'auto'}
+    )
 
 
 
 
-
-
-app = dash.Dash(__name__)
-#app.config.suppress_callback_exceptions = True
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.GRID])
+app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div(
     id="outer layout",
@@ -41,30 +85,89 @@ app.layout = html.Div(
                 layouts.build_tabs(),
                 # Main app
                 html.Div(id="app-content"),
+
             ],
+
         ),
+        # The following are helper components
         banner.generate_learn_button(),
+        banner.generate_settings_button(),
         dcc.Interval(
             id="interval-component",
-            interval=1000,  # 1 second
+            interval=5*1000,  # 5 seconds
             n_intervals=0,
         ),
+
     ],
 )
 
+
+
 @app.callback(
-    Output("markdown", "style"),
-    [Input("learn-more-button", "n_clicks"), Input("markdown_close", "n_clicks")],
+    Output("auto-toast", "is_open"), [Input("auto-toast-toggle", "n_clicks")]
 )
-def update_click_output(button_click, close_click):
+def open_toast(n):
+    return True
+
+
+
+@app.callback(
+    Output("learn", "style"),
+    [Input("learn-more-button", "n_clicks"),
+     Input("markdown_close", "n_clicks"),
+     ],
+)
+def trigger_learn_more(button_click, close_click):
     ctx = dash.callback_context
 
     if ctx.triggered:
         prop_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
         if prop_id == "learn-more-button":
             return {"display": "block"}
-
     return {"display": "none"}
+
+@app.callback(
+    Output("settings", "style"),
+    [Input("settings-button", "n_clicks"),
+     Input("settings-close", "n_clicks"),
+     ],
+)
+def trigger_settings(button_click, close_click):
+    ctx = dash.callback_context
+
+    if ctx.triggered:
+        prop_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+        if prop_id == "settings-button":
+            return {"display": "block"}
+    return {"display": "none"}
+
+
+layout1 = dbc.Container(html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(layouts.build_overview_tab(data_obj, fig1), width=10),
+                dbc.Col(stats_panel(), width=2),
+            ],
+            no_gutters=True,
+        ),
+    ]
+))
+
+layout2 = dbc.Container(html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col(layouts.build_sensors_tab(data_obj, fig2), width=10),
+                dbc.Col(stats_panel(), width=2),
+            ],
+            no_gutters=True,
+        ),
+    ]
+))
+
 
 
 @app.callback(
@@ -73,11 +176,11 @@ def update_click_output(button_click, close_click):
 )
 def render_tab_content(tab_switch):
     if tab_switch == "sensors":
-        return layouts.build_sensors_tab(data_obj, fig2)
+        return layout2
     elif tab_switch == 'overview':
-        return layouts.build_overview_tab(data_obj, fig1)
+        return layout1
     else:
-        return layouts.build_overview_tab(data_obj, fig1)
+        return layout1
 
 
 
@@ -106,7 +209,7 @@ def update_line_on_interval(counter):
     df = data_obj.data[id1]['data']
     return figures.line_figure(df)
 
-
+banner.register_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8051)
